b2779cea9021404c03a2ec2af07109b4e83fa874
sakari jokinen <sakariij@gmail.com>
2013-12-18 19:03:35 +0200

    save the expression location for type constraint

--- src/Language/Tip/Ast.hs
+++ src/Language/Tip/Ast.hs
@@ -5,10 +5,6 @@ import Data.Typeable
 import Data.Data
 import Text.Parsec hiding (getPosition)
 
-srcLine a = sourceLine a
-srcName a = sourceName a
-srcColumn a = sourceColumn a
-
 data Module = Module {
       moduleFilePath :: FilePath
     , moduleStatements :: [Statement]
--- src/Language/Tip/Types.hs
+++ src/Language/Tip/Types.hs
@@ -4,12 +4,15 @@ module Language.Tip.Types (infer) where
 import Data.Generics
 import Language.Tip.Symbols
 import qualified Language.Tip.Ast as Ast
+import Text.Parsec.Pos (newPos, SourcePos, sourceColumn, sourceName, sourceLine)
+
 import Language.Tip.Scope
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Control.Monad.State.Strict
 import Control.Applicative
 import Data.Maybe
+import Data.List hiding (minimum)
 import Prelude hiding (minimum)
 
 type Typed = Either [String] ()
@@ -24,45 +27,38 @@ infer Ast.Module { Ast.moduleStatements } = result
       vars = Set.toList $ declarations moduleStatements
 
 pretty :: TypingError -> String
-pretty NotSubtype { sub, super } = p sub ++ " is not a subtype of " ++ p super
+pretty NotSubtype { sub, super, at } = concat $ intersperse " " [
+                                        prettyType sub, "does not fit"
+                                       , prettyType super,  "at"
+                                       , position ]
     where
-      p o = "'" ++ (prettyType o) ++ "' at " ++ prettyPos o
+      position = sourceName at ++ ":" ++ show (sourceLine at) ++ " column " ++ show (sourceColumn at)
 
-prettyType = go . ty
+prettyType = go
     where
       go StringType = "string"
       go NumberType = "number"
 
-prettyPos o = Ast.srcName pos ++ ":" ++ show (Ast.srcLine pos)
-    where
-      pos = Ast.getPosition o
-
-instance Ast.Positioned Type where
-    getPosition ExpressionType { expr } = Ast.getPosition expr
-    getPosition StmtType { stmt } = Ast.getPosition stmt
-
-data Type = ExpressionType { expr :: Ast.Expression
-                           , ty :: Ty}
-          | StmtType { stmt :: Ast.Statement
-                     , ty :: Ty }
-            deriving (Show)
+data Constraint = Constraint { constraintPosition :: SourcePos, constraintType :: Ty }
+                deriving (Show)
 
 data Ty = StringType
         | NumberType
-        | Structure { structure :: Map.Map String Type }
-        | Function { parameters :: [Type], returnType :: Type }
+        | Structure { structure :: Map.Map String Ty }
+        | Function { parameters :: [Ty], returnType :: Ty }
         | Reference { ref :: Ref }
-        | ConstrainedType { constraints :: [Type]
+        | ConstrainedType { constraints :: [Constraint]
                           , minimum :: Maybe Ty }
           deriving (Show)
 
-data TypingError = NotSubtype { sub :: Type,  super :: Type }
+data TypingError = NotSubtype { sub :: Ty,  super :: Ty, at :: SourcePos }
                  deriving (Show)
 
 data Env = Env { errors :: [TypingError]
-               , env :: Map.Map Ref Ty }
+               , env :: Map.Map Ref Ty
+               , position :: SourcePos }
 
-emptyEnv = Env { errors = [], env = Map.empty }
+emptyEnv = Env { errors = [], env = Map.empty, position = newPos "" 0 0  }
 
 newtype TypingM a = TypingM { typingM :: ScopeT (State Env) a }
     deriving (Monad, Functor, ScopeMonad)
@@ -74,28 +70,44 @@ instance MonadState Env TypingM where
 runTyping :: TypingM a -> Env
 runTyping t = snd $ ( runState $ runScope $ typingM t) emptyEnv
 
+currentPosition :: TypingM SourcePos
+currentPosition = do
+  gets position
+
+atPosition :: SourcePos -> TypingM a -> TypingM a
+atPosition p m = do
+  previous <- currentPosition
+  modify $ w p
+  a <- m
+  modify $ w previous
+  return a
+    where
+      w p e = e { position = p }
+
 typeStmt :: Ast.Statement -> TypingM ()
 typeStmt Ast.Statement { Ast.stmt = Ast.ExpressionStmt { Ast.expression }} =
     typeExpression expression >> return ()
 typeStmt Ast.Statement { Ast.stmt = Ast.VarStmt {}} = return ()
 typeStmt stmt = error $ "missing typeStmt case for: " ++ show stmt
 
-typeExpression e@Ast.Expression { Ast.expr, Ast.exprType } = go expr >>= typed exprType
+typeExpression e@Ast.Expression { Ast.expr, Ast.exprType } = atPosition pos $
+                                                             go expr >>= typed exprType
     where
+      pos = Ast.getPosition e
       typed Nothing x_type = return x_type
       typed (Just t) x_type = do
         assignedType <- typeLiteral t
-        (x_type', assigned') <- x_type |> x_type { ty = assignedType }
+        (x_type', assigned') <- x_type |> assignedType
         return assigned'
 
       go Ast.Identifier { Ast.identifierName } = do
         resolved <- resolve identifierName
         case resolved of
           Nothing -> error $ "could not resolve identifier: " ++ identifierName
-          Just i -> return $ ExpressionType e $ Reference i
+          Just i -> return $ Reference i
 
-      go Ast.StringLiteral {} = return $ ExpressionType e StringType
-      go Ast.IntLiteral {} = return $ ExpressionType e NumberType
+      go Ast.StringLiteral {} = return StringType
+      go Ast.IntLiteral {} = return NumberType
       go Ast.Assignment { Ast.lhs, Ast.rhs } = do
         lhs_t <- typeExpression lhs
         rhs_t <- typeExpression rhs
@@ -106,8 +118,8 @@ typeExpression e@Ast.Expression { Ast.expr, Ast.exprType } = go expr >>= typed e
         t <- typeExpression callee
         args <- mapM typeExpression arguments
         r <- fresh
-        let ref = ExpressionType { expr = e, ty = Reference { ref = r } }
-            c = ExpressionType { expr = e, ty = Function { parameters = args, returnType = ref }}
+        let ref = Reference { ref = r }
+            c = Function { parameters = args, returnType = ref }
         c |> t
         return ref
       go x = error $ "typeExpression missing case for: " ++ show x
@@ -131,61 +143,68 @@ asserttype r t = modify w >> return t
     where
       w e@Env { env } = e { env = Map.insert r t env }
 
-mismatch :: Type -> Type -> TypingM ()
+mismatch :: Ty -> Ty -> TypingM ()
 mismatch l r = modify w
     where
-      w e@Env { errors } = e { errors = k:errors }
-      k = NotSubtype { super = l, sub = r }
+      w e@Env { errors, position } = e { errors = k position:errors }
+      k p = NotSubtype { super = l, sub = r, at = p }
 
-(|>) :: Type -> Type -> TypingM (Type, Type)
-l |> r = do
-  (ty_l, ty_r) <- go (ty l) (ty r)
-  return (l { ty = ty_l }, r { ty = ty_r })
+(|>) :: Ty -> Ty -> TypingM (Ty, Ty)
+l |> r = go l r
     where
       go var@Reference { ref } rhs = do
         refType <- typeof ref
-        (l', r') <- l { ty = refType } |> r
-        asserttype ref $ ty l'
-        return (var, ty r')
+        (l', r') <- refType |> r
+        asserttype ref l'
+        return (var, r')
 
       go lhs var@Reference { ref } = do
         refType <- typeof ref
-        (l', r') <- l |> r { ty = refType }
-        asserttype ref $ ty r'
-        return (ty l', var)
+        (l', r') <- l |> refType
+        asserttype ref $ r'
+        return (l', var)
 
       go StringType StringType = return (StringType, StringType)
       go NumberType NumberType = return (NumberType, NumberType)
       go StringType NumberType = mismatch l r >> return (StringType, NumberType)
       go NumberType StringType = mismatch l r >> return (StringType, NumberType)
 
-      go c@ConstrainedType { minimum = Nothing, constraints } _ = return (c_new, ty r)
-          where
-            c_new = c { constraints = r:constraints }
+      go c@ConstrainedType { minimum = Nothing, constraints } _ = do
+        r_c <- constraint r
+        return (c { constraints = r_c : constraints }, r)
 
       go c@ConstrainedType { minimum = Just t, constraints } _ = do
-        (l', r') <- l { ty = t }  |> r
-        let c_new = c { minimum = Just $ ty l', constraints = r':constraints }
-        solve $ l { ty = c_new }
-        return (c_new, ty r')
+        (l', r') <- t |> r
+        r_c <- constraint r'
+        let c_new = c { minimum = Just l', constraints = r_c:constraints }
+        solve c_new
+        return (c_new, r')
 
       go l c@ConstrainedType { minimum = Nothing, constraints } = do
-        solve $ r { ty = c_new }
+        solve c_new
         return (l, c_new)
           where
             c_new = c { minimum = Just l }
 
       go _ c@ConstrainedType { constraints, minimum = Just t } = do
-        (l', t') <- l |> r { ty = t }
-        let c_new = c { minimum = Just $ ty t' }
-        solve $ r { ty = c_new }
-        return (ty l', c_new)
+        (l', t') <- l |> t
+        let c_new = c { minimum = Just t' }
+        solve c_new
+        return (l', c_new)
 
       go lhs rhs = mismatch l r >> return (lhs, rhs)
 
-solve :: Type -> TypingM ()
-solve t = go $ ty t
+constraint :: Ty -> TypingM Constraint
+constraint t = do
+  p <- currentPosition
+  return $ Constraint { constraintPosition = p, constraintType = t}
+
+solve :: Ty -> TypingM ()
+solve t = go t
     where
+      w m Constraint { constraintPosition, constraintType } =
+          atPosition constraintPosition $
+                     m |> constraintType
       go ConstrainedType { minimum = Just m, constraints } =
-          mapM_ (t { ty = m }  |>) constraints
+          mapM_ (w m) constraints
       go _ = return ()
